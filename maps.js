5ed43914e0b6e9f03d9ef15a6deb7319d40729f3
alex <alex.hmn18@gmail.com>
2025-02-18 11:52:19 +0100

    feat: affichage des maps

--- maps.js
+++ maps.js
@@ -5,7 +5,21 @@ async function afficherMaps() {
 
 (async () => {
     let { data } = await afficherMaps();
-    data.forEach(map => 
-        console.log(map)
-    );
+    let rowMaps = document.querySelector('#maps .row');
+
+    data.forEach(map => {
+        let colElement = document.createElement('div');
+        colElement.classList.add('col-lg-3', 'col-6');
+        rowMaps.appendChild(colElement);
+
+        let linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
+        linkElement.style.backgroundImage = `url(${map.splash})`;
+        linkElement.className = ('linkMaps');
+        colElement.appendChild(linkElement);
+    
+        let titleElement = document.createElement('h3');
+        titleElement.textContent = (map.displayName)
+        linkElement.appendChild(titleElement);
+    });
 })();
