88222e81f4fc8aa014ff1559279a1bc23c3cc3a8
alex <alex.hmn18@gmail.com>
2025-02-18 12:28:16 +0100

    fix: fix du bug affichage des maps

--- maps.js
+++ maps.js
@@ -1,25 +1,38 @@
-async function afficherMaps() {
-    let response = await fetch('https://valorant-api.com/v1/maps?language=fr-FR');
-    return await response.json();
-}
+document.addEventListener('DOMContentLoaded', () => {
+    async function afficherMaps() {
+        let response = await fetch('https://valorant-api.com/v1/maps?language=fr-FR');
+        return await response.json();
+    }
 
-(async () => {
-    let { data } = await afficherMaps();
-    let rowMaps = document.querySelector('#maps .row');
+    document.querySelector('nav ul li a[href="#maps"]').addEventListener('click', (event) => {
+        event.preventDefault();
+        (async () => {
+            let { data } = await afficherMaps();
+            let rowMaps = document.querySelector('#Container');
+            rowMaps.innerHTML = "";
 
-    data.forEach(map => {
-        let colElement = document.createElement('div');
-        colElement.classList.add('col-lg-3', 'col-6');
-        rowMaps.appendChild(colElement);
+            let titreElement = document.createElement('h2');
+            titreElement.classList.add('mb-4');
+            titreElement.textContent = "MAPS";
+            rowMaps.appendChild(titreElement);
 
-        let linkElement = document.createElement('a');
-        linkElement.setAttribute('href', '#');
-        linkElement.style.backgroundImage = `url(${map.splash})`;
-        linkElement.className = ('linkMaps');
-        colElement.appendChild(linkElement);
-    
-        let titleElement = document.createElement('h3');
-        titleElement.textContent = (map.displayName)
-        linkElement.appendChild(titleElement);
+            data.forEach(map => {
+                let colElement = document.createElement('div');
+                colElement.classList.add('col-lg-3', 'col-6');
+                rowMaps.appendChild(colElement);
+
+                let linkElement = document.createElement('a');
+                linkElement.setAttribute('href', '#');
+                linkElement.style.backgroundImage = `url(${map.splash})`;
+                linkElement.classList.add('linkMaps');
+                colElement.appendChild(linkElement);
+            
+                let titleElement = document.createElement('h3');
+                titleElement.textContent = map.displayName;
+                linkElement.appendChild(titleElement);
+            });
+        })();
     });
-})();
+});
+
+
